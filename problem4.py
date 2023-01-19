958a23e2afc1eba457b93ec6de62a3b94b1bdf30
ZilchofNowhere <81156765+ZilchofNowhere@users.noreply.github.com>
2023-01-19 12:29:19 +0300

    Add files via upload

--- problem4.py
+++ problem4.py
@@ -1,32 +1,32 @@
-a: float = 3
-b: float = 5
-c: float = 7
-d: float = 4
-e: float = 10
+xa: float = 0
+ya: float = 2
+xd: float = 4
+yd: float = 2
+xc: float = 6
 
 def problem4(X):
-    u = X[0]
-    v = X[1]
-    m = X[2]
+    m = X[0]
+    xe = X[1]
+    yf = X[2]
     global a, b, c, d, e
 
-    x1 = (m**2 * u + m * v) / (m**2 + 1)
-    y1 = m * (x1 - u)
+    xh = (m**2 * xe + m * yf) / (m**2 + 1)
+    yh = (m**2 * yf - m * xe) / (m**2 + 1)
 
-    x2 = m * a * v / (b * m + a)
-    y2 = (b / a) * x2
+    xj = (ya * xd - xa * xd - m * xe * (xa - xd)) / (ya - yd - m * xa + m * xd)
+    yj = m * (ya * xd - xa * xd - xe * ya + xe * yd) / (ya - yd - m * xa + m * xd)
 
-    x3 = ((c - a) * (-m * v - b) + a*d - a*b) / (d - b - m*c + m*a)
-    y3 = m * (x3 - u)
+    xg = m * xa * yf / (m * ya + xa)
+    yg = m * ya * yf / (m * ya + xa)
 
-    x4 = m * (v * c - e * v + e * d) / (d * m + c - e)
-    y4 = d / (c-e) * (x4 - e)
+    xi = (m * xc * yf - xd * m * yf - m * yd * xc) / (xc - xd - m * yd)
+    yi = (yd * xc - m * yd * yf) / (xc - xd - m * yd)
 
-    s = abs(e * d + b * c - a * d) / 2
-    s1 = abs(a * y2 + x2 * y1 + x1 * y3 + x3 * b - a * y3 - x3 * y1 - x1 * y2 - x2 * b) / 2
-    s2 = abs(u * y1 + x1 * y2 - x2 * y1) / 2
-    s3 = abs(c * y4 + x4 * y1 - x1 * y4 - u * y1) / 2
-    s4 = abs(x3 * y1 + x1 * y4 + x4 * d + c * y3 - x3 * d - c * y4 - x4 * y1 - x1 * y3) / 2
+    s = abs((xc * yd + xd * ya) - (xa * yd)) / 2
+    s1 = abs(xe * ((m**2 * yf - m * xe) / (m**2 + 1)) + ((m**2 * xe + m * yf) / (m**2 + 1)) * (m * ya * yf / (m * ya + xa)) - ((m * xa * yf / (m * ya + xa)) * (m**2 * yf - m * xe) / (m**2 + 1))) / 2
+    s2 = abs((xc * (yd * xc - m * yd * yf) / (xc - xd - m * yd) + (m * xc * yf - xd * m * yf - m * yd * xc) / (xc - xd - m * yd) * (m**2 * yf - m * xe) / (m**2 + 1)) - (xe * (m**2 * yf - m * xe) / (m**2 + 1) + (m**2 * xe + m * yf) / (m**2 + 1) * (yd * xc - m * yd * yf) / (xc - xd - m * yd))) / 2
+    s3 = abs((xj * yh + xh * yi + xi * yd + xd * yj) - (xh * yj + xi * yh + xd * yi + xj * yd)) / 2
+    s4 = abs((xa * yg + xg * yh + xh * yj + xj * ya) - (xg * ya + xh * yg + xj * yh + xa * yj)) / 2
 
     q = s / 4
     return abs(s1 - q) + abs(s2 - q) + abs(s3 - q) + abs(s4 - q)
