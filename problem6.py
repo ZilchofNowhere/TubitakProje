e02e7f69dac4b21e75330af82acca63722dfaea6
Efe Atakan Karacan <78875992+efeatakankaracan@users.noreply.github.com>
2023-01-19 13:14:27 +0300

    Add files via upload

--- problem6.py
+++ problem6.py
@@ -16,10 +16,23 @@ def problem6(X):
     xh=((m**2)*xf+xg)/((m**2)+1)
     yh=(m(xg-xf))/(m**2+1)
 
+    s = 2* (a**3) / 3
+
     s1=abs((xf*(m*(xg-xf)/(m**2 + 1))) + (m**2 * xf + xg)/(m**2 + 1)*(xg/m))/2
     s2=abs((xg* (m(xg-xf))/(m**2+1)) - (xf * (m(xg-xf)/(m**2+1))))/2
     s3a=abs((((-m+sqrt(m**2+(4*(m*xf+a**2))))/2)* (m*(xg-xf)) / (m**2+1) + (m*a*(-m+sqrt(m**2 + 4 * (m*xf + a**2)))/2 - xf)) - 
-    )
+    (((m**2 * xf + xg) / (m**2+1)) * m * (-m * sqrt((m**2+ 4 * (m * xf + a**2)))/ 2 - xf)) + xg * (m*(xg-xf)/(m**2+1)))
 
+    s3b=2*(a**3)/3 - (a**2 * (-m + sqrt(m**2 + 4 * (m*xf + a**2))) / 2 + ((-m + sqrt(m**2 + 4*(m*xf+ a**2))) / 2) / 3) - m * a
+    ((-m + sqrt(m**2+4*(m*xf+a**2))) / 2 -xf) + (m * ((-m+sqrt(m**2 + 4*(m*xf+ a**2))) / 2 - xf) * ((-m+sqrt(m**2+4*(m*xf+a**2)))/2))/2
 
-    return abs(s1 - q) + abs(s2 - q) + abs(s3 - q) + abs(s4 - q)
+    s3=s3a+s3b
+
+    s4a=abs((m**2 * xf + xg)/ (m**2+1) * m *( (-m + sqrt(m**2 + 4 * (m * xf + a ** 2))) / 2 - xf) + (-m + sqrt(m**2 + 4 * (xf+ a**2)))/ 2
+    * a**2 - ((m**2 * xf + xg)/ (m**2+1) * (xg/m) + ((-m+sqrt(m**2+4*(xf+a**2)))/2 * (m * (xg-xf)) / (m**2+1))))
+    
+    s4b=(a**2 * (-m+sqrt(m**2+ 4 * (m*xf+a**2)))/2 - (((-m+(sqrt(m**2+ 4 * (m*xf+a**2)))) / 2) / 3)  ) - (((a**2+m * (-m+sqrt(m**2+ 4 *(m * xf + a**2))) / 2 - xf)) * ((-m+sqrt(m**2+4*(m*xf+a**2))) / 2)/2)
+
+    s4=s4a+s4b
+
+    return abs(s1-(s/18)) + abs(s2-(s/9)) + abs(s3-(s/6)) + abs(s4-(2*s/3)) 
